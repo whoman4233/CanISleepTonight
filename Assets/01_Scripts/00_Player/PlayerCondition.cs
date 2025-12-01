dcd5cc4d28219fe8873e49113a0bdbac097d6ad8
arara0250 <arara4281@gmail.com>
2025-12-01 20:08:38 +0900

    feat : Player 스테이터스 기능 추가
    - UI 업데이트 로직 (Test UI) 포함

--- Assets/01_Scripts/00_Player/PlayerCondition.cs
+++ Assets/01_Scripts/00_Player/PlayerCondition.cs
@@ -1,12 +1,61 @@
-﻿using System;
+using System;
 using UnityEngine;
 
 public class PlayerCondition : MonoBehaviour
 {
-
     private bool isLife = true;
 
     //Condition fatigue { get { return uICondition.health; } }
     //Condition stress { get { return uICondition.hunger; } }
 
+    [SerializeField] private float stress;
+    [SerializeField] private float fatigue;
+    [SerializeField] private float maxValue;
+
+    private void Start()
+    {
+        // 시작 시 UI값 초기 반영
+        UpdateStressUI();
+        UpdateFatigueUI();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            AddStress(10);
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+            AddFatigue(30);
+    }
+
+    public void AddStress(float value)
+    {
+        stress = Mathf.Clamp(stress + value, 0, 100);
+        UpdateStressUI();
+    }
+
+    public void AddFatigue(float value)
+    {
+        fatigue = Mathf.Clamp(fatigue + value, 0, 100);
+        UpdateFatigueUI();
+    }
+
+    private void UpdateStressUI()
+    {
+        if (UIManager.Instance != null)
+        {
+            UIManager.Instance.StressBar.fillAmount = stress / maxValue;
+        }
+
+        Debug.Log($"스트레스 : {stress} / {maxValue}");
+    }
+
+    private void UpdateFatigueUI()
+    {
+        if (UIManager.Instance != null)
+        {
+            UIManager.Instance.FatigueBar.fillAmount = fatigue / maxValue;
+        }
+
+        Debug.Log($"피로 : {fatigue} / {maxValue}");
+    }
 }
--- Assets/01_Scripts/01_Manager/UIManager.cs
+++ Assets/01_Scripts/01_Manager/UIManager.cs
@@ -1,11 +1,21 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     [Header("TestUI 오브젝트")]
     [SerializeField] private TextMeshProUGUI promptText;
     public TextMeshProUGUI PromptText => promptText;
+
+    [SerializeField] private Image stressBar;
+
+    public Image StressBar => stressBar;
+
+    [SerializeField] private Image fatigueBar;
+
+    public Image FatigueBar => fatigueBar;
+
 
     public static UIManager Instance { get; private set; }
 
