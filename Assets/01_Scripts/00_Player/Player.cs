60a843c76683364bd5a7a42b1fcc7955fbd74d05
arara0250 <arara4281@gmail.com>
2025-12-02 20:14:17 +0900

    feat : Player.cs > 인벤토리 (Item 리스트) 및 장착 아이템 검색 함수 추가

--- Assets/01_Scripts/00_Player/Player.cs
+++ Assets/01_Scripts/00_Player/Player.cs
@@ -1,4 +1,6 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 public class Player : MonoBehaviour
 {
@@ -11,11 +13,19 @@ public class Player : MonoBehaviour
 
     public Transform dropPosition;
 
+    private List<Item> inventory = new List<Item>();
+    public List<Item> Inventory {  get { return inventory; } }
+
     private void Awake()
     {
         PlayerManager.Instance.Player = this;
         controller = GetComponent<PlayerController>();
         condition = GetComponent<PlayerCondition>();
         //equip = GetComponent<Equipment>();
+    }
+
+    public Item GetEquippedItem()
+    {
+        return inventory.FirstOrDefault(item => item.IsEquipped);
     }
 }
