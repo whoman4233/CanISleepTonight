cce1aa24e98c7a6cf26af0832e4bd53e24b50183
arara0250 <arara4281@gmail.com>
2025-12-02 20:54:17 +0900

    feat : 아이템 설명창 버튼 텍스트 갱신 로직 추가

--- Assets/01_Scripts/02_UI/UIItemDetail.cs
+++ Assets/01_Scripts/02_UI/UIItemDetail.cs
@@ -4,9 +4,18 @@ using UnityEngine.UI;
 
 public class UIItemDetail : MonoBehaviour
 {
+    [Header("Item Detail UI")]
     [SerializeField] private Image iconImage;
     [SerializeField] private TextMeshProUGUI nameText;
     [SerializeField] private TextMeshProUGUI descText;
+
+    [Header("EquipButton")]
+    [SerializeField] private Button EquipButton;
+    [SerializeField] private TextMeshProUGUI equipButtonText;
+
+    [Header("PurchaseButton")]
+    [SerializeField] private Button PurchaseButton;
+    [SerializeField] private TextMeshProUGUI purchaseButtonText;
 
     public void Show(Item item)
     {
@@ -14,11 +23,45 @@ public class UIItemDetail : MonoBehaviour
         nameText.text = item.ItemData.itemName;
         descText.text = item.ItemData.itemDescription;
 
+        if (item.IsLocked)
+            SetPurchaseButton(item);
+        else
+            SetEquipButton(item);
+
         gameObject.SetActive(true);
+    }
+
+    private void SetEquipButton(Item item)
+    {
+        if (item.IsEquipped)
+            equipButtonText.text = "해제";
+        else
+            equipButtonText.text = "장착";
+
+        EquipButton.gameObject.SetActive(true);
+        PurchaseButton.gameObject.SetActive(false);
+    }
+
+    private void SetPurchaseButton(Item item)
+    {
+        purchaseButtonText.text = $"구매 : {item.ItemData.itemPrice}\u20a9";
+
+        PurchaseButton.gameObject.SetActive(true);
+        EquipButton.gameObject.SetActive(false);
     }
 
     public void Hide()
     {
         gameObject.SetActive(false);
+    }
+
+    public void OnEquipButtonClicked()
+    {
+
+    }
+
+    public void OnPurchaseButtonClicked()
+    {
+
     }
 }
